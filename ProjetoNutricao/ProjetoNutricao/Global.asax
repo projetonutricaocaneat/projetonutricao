1fcecd60418e3d0f921ff667c2ac78ee5b5f14ca
jparaujorocha <desenvolvimento@interweb.net.br>
2016-07-26 12:03:42 -0300

    Primeiro Commit

---
+++ ProjetoNutricao/ProjetoNutricao/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetoNutricao.MvcApplication" Language="C#" %>
---
+++ ProjetoNutricao/ProjetoNutricao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoNutricao.Startup))]
+namespace ProjetoNutricao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
